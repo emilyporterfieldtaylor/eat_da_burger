37801bc0268a8cb8fd893b1123bfad5ba23baf48
Emily Taylor <emilyporterfieldtaylor@gmail.com>
2020-03-15 17:34:40 -0500

    change from cats to burgers

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,3 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Impossible Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss', true);
