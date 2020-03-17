1c50bb9acdad1cf29d4b2c5afa455a699f8dcae0
Emily Taylor <emilyporterfieldtaylor@gmail.com>
2020-03-17 09:51:43 -0500

    ignore this commit. DB is wrong

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,17 @@
 var mysql = require("mysql");
 require('dotenv').config();
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Cal&Sof20!",
-  database: "burgers_db"
-});
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+   connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.DB_PASS,
+    database: "cat_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
